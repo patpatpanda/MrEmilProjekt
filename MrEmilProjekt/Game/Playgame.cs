fae49f969072f2770b3cd150a3b159112c0277da
Emil arrenius <Nils-Emil1337@hotmail.se>
2023-01-10 12:51:55 +0100

    Skapat RockPaperScissor class

---
+++ MrEmilProjekt/Game/Playgame.cs
@@ -0,0 +1,41 @@
+﻿using MrEmilProjekt.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MrEmilProjekt.Game
+{
+    public class Playgame
+    {
+        public AppDbContext myContext;
+
+        public Playgame(AppDbContext context)
+        {
+            myContext = context;
+        }
+        public void PlayGame()
+        {
+            string player;
+            string computer;
+
+            int randomNumber;
+
+            Console.Write("ROCK,PAPER or SCISSOR : ");
+
+            Random myRandom = new Random();
+
+            randomNumber = myRandom.Next(1, 4);
+
+            switch (randomNumber)
+            {
+                case 1:
+                    computer = "ROCK";
+                    Console.WriteLine("Computer chose ");
+                    break;
+            }
+
+        }
+    }
+}
---
+++ MrEmilProjekt/Game/RockPaperScissor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MrEmilProjekt.Game
+{
+    internal class RockPaperScissor
+    {
+        public int Result { get; set; }
+        public int AverageWin { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
