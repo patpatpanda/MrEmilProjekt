a6dd55abe8a0b2fa92a22fbbeaaf542d13f6a98f
Emil arrenius <Nils-Emil1337@hotmail.se>
2023-01-10 12:12:54 +0100

    Skapat ett interface och en CreateCalculation Class som använder interfacet

---
+++ MrEmilProjekt/Calculator/ICalculation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MrEmilProjekt.Calculator
+{
+    public interface ICalculation
+    {
+        Calculator AdditonMaker();
+        Calculator SqrtMaker();
+        Calculator DividedMaker();
+        Calculator ModuloMaker();
+        Calculator MultiplyMaker();
+        Calculator SubtractionMaker();
+    }
+}
--- MrEmilProjekt/Calculator/OperatorMenu.cs
+++ MrEmilProjekt/Calculator/OperatorMenu.cs
@@ -14,12 +14,14 @@ namespace MrEmilProjekt.Calculator
         {
             var buildApp = new Builder();
             var myContext = buildApp.AppBuilder();
-            var multiply = new Multiply(myContext);
-            var subtraction = new Subtraction(myContext);
-            var sqrt = new Sqrt(myContext);
-            var divided = new Divided(myContext);
-            var addition = new Addition(myContext);
-            var modulo = new Modulo(myContext);
+            CalculationFactory factory = new CalculationFactory();
+            
+            var createCalculation = new CreateCalculation(factory,myContext);
+            
+            
+          
+            
+           
             var controllerCalc = new CalculatorController(myContext);
 
 
@@ -45,16 +47,15 @@ namespace MrEmilProjekt.Calculator
 
             if (input == "*")
             {
-                multiply.MultiplyCalculator();
+                createCalculation.MultiplyCalculator();
             }
-            else if (input == "+") addition.AdditonCalculator();
+            else if (input == "+") createCalculation.AdditonCalculator();
+            else if (input == "-") createCalculation.SubtractionCalculator();
+            else if (input == "/") createCalculation.DividedCalculator();
 
-                    else if (input == "-") subtraction.SubtractionCalculator();
-                    else if (input == "/") divided.DividedCalculator();
 
-
-                  
-                    else if (input == "%") modulo.ModuloCalculator();
+            else if (input == "%") createCalculation.ModuloCalculator();
+            else if(input == "1") createCalculation.SqrtCalculator();
                    
 
                     else if (input == "0")
